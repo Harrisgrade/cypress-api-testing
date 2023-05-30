081ee5a91fa7aa46281dcb7ef45213e42bc520d5
Harrisgrade <56458322+Harrisgrade@users.noreply.github.com>
2023-05-30 16:30:38 -0500

    query-test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,13 +1,35 @@
 // <reference types="cypress" />
+const result = {
+name: "Rick Sanchez",
+status: "Alive",
+species: "Human",
+type: "",
+gender: "Male",
+};
 
 describe('template spec', () => {
   it('passes', () => {
     cy.request({
       method: 'GET',
-      url: 'https://rickandmortyapi.com/graphql/?query=%7B%0A%20%20character(id%3A%201)%20%7B%0A%20%20%20%20name%0A%20%20%20%20status%0A%20%20%20%20species%0A%20%20%20%20type%0A%20%20%20%20gender%0A%20%20%20%20image%0A%20%20%20%20episode%20%7B%0A%20%20%20%20%20%20name%0A%20%20%20%20%7D%0A%20%20%20%20created%0A%20%20%7D%0A%7D',
+      url: 'https://rickandmortyapi.com/graphql/',
+      qs: {
+        query: `{
+          character(id: 1) {
+            name
+            status
+            species
+            type
+            gender
+          }
+        }`
+      }
     }).then((response) => {
-      console.log(response.body);
-      // cy.wrap(response).invoke('Message');
+      expect(response.body.data.character.name).to.equal(result.name);
+      cy.wrap(response.body.data.character.status).should('contain', result.status);
+      cy.wrap(response.body.data.character.species).should('contain', result.species);  
+    })
+   
   })
-  })  
 })
+
+     
